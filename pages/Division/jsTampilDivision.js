0d506e2a05d54223142421d4c3165fd1d9f2f716
FIDA TAUFIQ IMAZUDDIN <133633223+DevByFidaTaufiq@users.noreply.github.com>
2023-06-10 09:18:38 +0700

    deleted:    assets/js/jsTampilStatus.js 	new file:   pages/ClientCompany/jsTampilClientCompany.js 	new file:   pages/ClientCompany/tampilClientCompany.html 	new file:   pages/Division/jsTampilDivision.js 	new file:   pages/Division/tampilDivision.html 	new file:   pages/Position/jsTampilPosition.js 	new file:   pages/Position/tampilPosition.html 	new file:   pages/Role/jsTampilRole.js 	new file:   pages/Role/tampilRole.html 	new file:   pages/StatusProject/jsTampilStatus.js 	new file:   pages/StatusProject/tampilStatus.html 	modified:   pages/dashboard.html 	deleted:    pages/tampilStatus.html
       saya mengerjakan "ClientCompany/Division/Position/Role"
       yang belum "Team/Employee/Project"

---
+++ pages/Division/jsTampilDivision.js
@@ -0,0 +1,41 @@
+const allSideMenu = document.querySelectorAll("#sidebar .side-menu.top li a");
+
+allSideMenu.forEach((item) => {
+   const li = item.parentElement;
+
+   item.addEventListener("click", function () {
+      allSideMenu.forEach((i) => {
+         i.parentElement.classList.remove("active");
+      });
+      li.classList.add("active");
+   });
+});
+
+// TOGGLE SIDEBAR
+const menuBar = document.querySelector("#content nav .fa-solid.fa-bars");
+const sidebar = document.getElementById("sidebar");
+
+menuBar.addEventListener("click", function () {
+   sidebar.classList.toggle("hide");
+});
+
+function swalSuccess() {
+   swal("Success!", "Data telah ditambahkan !", "success");
+   setTimeout(function () {
+      location.reload(); // Reload halaman setelah 1,7 detik menjalankan fungsi
+   }, 1700);
+}
+
+function swalWarn() {
+   swal("Warning!", "Pastikan semua kolom telah terisi !", "warning");
+}
+
+function Submit(event) {
+   event.preventDefault(); // Agar halaman tidak ter-reload pada klik submit form
+   const dataStatus = document.getElementById("sDivision").value;
+   if (dataStatus === "") {
+      swalWarn();
+   } else {
+      swalSuccess();
+   }
+}
