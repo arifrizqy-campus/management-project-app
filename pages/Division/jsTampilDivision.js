b6a5b925544281b337587e5615a308893a847991
avrzll <220441100149@student.trunojoyo.ac.id>
2023-06-11 23:00:51 +0700

    ubah bahasa

--- pages/Division/jsTampilDivision.js
+++ pages/Division/jsTampilDivision.js
@@ -1,14 +1,14 @@
 const allSideMenu = document.querySelectorAll("#sidebar .side-menu.top li a");
 
 allSideMenu.forEach((item) => {
-   const li = item.parentElement;
+  const li = item.parentElement;
 
-   item.addEventListener("click", function () {
-      allSideMenu.forEach((i) => {
-         i.parentElement.classList.remove("active");
-      });
-      li.classList.add("active");
-   });
+  item.addEventListener("click", function () {
+    allSideMenu.forEach((i) => {
+      i.parentElement.classList.remove("active");
+    });
+    li.classList.add("active");
+  });
 });
 
 // TOGGLE SIDEBAR
@@ -16,26 +16,26 @@ const menuBar = document.querySelector("#content nav .fa-solid.fa-bars");
 const sidebar = document.getElementById("sidebar");
 
 menuBar.addEventListener("click", function () {
-   sidebar.classList.toggle("hide");
+  sidebar.classList.toggle("hide");
 });
 
 function swalSuccess() {
-   swal("Success!", "Data telah ditambahkan !", "success");
-   setTimeout(function () {
-      location.reload(); // Reload halaman setelah 1,7 detik menjalankan fungsi
-   }, 1700);
+  swal("Success!", "Data has been added !", "success");
+  setTimeout(function () {
+    location.reload(); // Reload halaman setelah 1,7 detik menjalankan fungsi
+  }, 1700);
 }
 
 function swalWarn() {
-   swal("Warning!", "Pastikan semua kolom telah terisi !", "warning");
+  swal("Warning!", "Make sure all fields are filled in !", "warning");
 }
 
 function Submit(event) {
-   event.preventDefault(); // Agar halaman tidak ter-reload pada klik submit form
-   const dataStatus = document.getElementById("sDivision").value;
-   if (dataStatus === "") {
-      swalWarn();
-   } else {
-      swalSuccess();
-   }
+  event.preventDefault(); // Agar halaman tidak ter-reload pada klik submit form
+  const dataStatus = document.getElementById("sDivision").value;
+  if (dataStatus === "") {
+    swalWarn();
+  } else {
+    swalSuccess();
+  }
 }
